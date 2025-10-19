dce96d9bf8013b721011e9d116d662fec93f36c6
Omar <omarmzoair@gmail.com>
2025-10-19 23:00:55 +0300

    restoring files

---
+++ MainComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "PlayerGUI.h"
+
+class MainComponent : public juce::AudioAppComponent
+{
+public:
+    MainComponent();
+    ~MainComponent() override;
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources() override;
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+
+private:
+    PlayerGUI player1;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+
+};
